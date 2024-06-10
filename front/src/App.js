eff9209c5762a1c190d69e1165dcff76b0b151e7
HW <hyeongu0305@gmail.com>
2024-06-10 17:40:25 +0900

    포맷팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const UserList = () => {
-  const [news, setNews] = useState();
+    const [news, setNews] = useState()
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await axios.get('news?question=bitcoin')
-        if ( Array.isArray(response.data.top_headlines.articles) && response.data.top_headlines.articles.length > 0) {
-          setNews(response.data.top_headlines.articles[0])
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const response = await axios.get('news?question=bitcoin')
+                if (
+                    Array.isArray(response.data.top_headlines.articles) &&
+                    response.data.top_headlines.articles.length > 0
+                ) {
+                    setNews(response.data.top_headlines.articles[0])
+                }
+            } catch (error) {
+                console.error('Error fetching users:', error)
+            }
         }
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
 
-    fetchNews();
-  }, []);
+        fetchNews()
+    }, [])
 
-  return (
-    <div>
-      <h2>News Data</h2>
-      <div>
-        <div>name : {news?.source.name}</div>
-        <div>author : {news?.author}</div>
-        <div>title : {news?.title}</div>
-        <div>publishedAt : {news?.publishedAt}</div>
-      </div>
-      
-    </div>
-  );
-};
+    return (
+        <div>
+            <h2>News Data</h2>
+            <div>
+                <div>name : {news?.source.name}</div>
+                <div>author : {news?.author}</div>
+                <div>title : {news?.title}</div>
+                <div>publishedAt : {news?.publishedAt}</div>
+            </div>
+        </div>
+    )
+}
 
-export default UserList;
+export default UserList
