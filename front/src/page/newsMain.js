39e5949e0688bb5fe2ca859b3a1645374d7f3992
HW <hyeongu0305@gmail.com>
2024-06-12 17:55:58 +0900

    메인, 리스트 화면 생성 및 버튼 이미지 추가

--- front/src/page/newsMain.js
+++ front/src/page/newsMain.js
@@ -4,6 +4,21 @@ import './newsMain.css'
 const Main = () => {
     return (
         <div className='mainWrap'>
+            <div className='content'>
+                <div className='titleWrap'>
+                    <div className='title'>사람들이 가장 좋아한 글</div>
+                    <div className='slideWrap'>
+                        <img className='prevBtn' src="./static/prevBtn.png" alt="" />
+                        <img className='nextBtn' src="./static/nextBtn.png" alt="" />
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
