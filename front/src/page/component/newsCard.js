6b4cead6b27b0dc51a1a3ba20795784c8009f546
Ruah Kim <kimhk0315@gmail.com>
2024-06-13 14:47:24 +0900

    🎨 truncate summary

--- front/src/page/component/newsCard.js
+++ front/src/page/component/newsCard.js
@@ -2,13 +2,20 @@
 import React from "react";
 import "./newsCard.css";
 
-const NewsCard = ({ image, title, publishedAt, content }) => {
+const NewsCard = ({ image, title, publishedAt, summary }) => {
+  const truncateText = (text, limit) => {
+    if (text.length > limit) {
+      return text.substring(0, limit) + "...";
+    }
+    return text;
+  };
+
   return (
     <div className="news-card">
       <img src={image} alt={title} className="news-card__image" />
       <div className="news-card__content">
         <h4 className="news-card__title">{title}</h4>
-        <p className="news-card__snippet">{content}</p>
+        <p className="news-card__snippet">{truncateText(summary, 58)}</p>
         <p className="news-card__date">{publishedAt}</p>
       </div>
     </div>
--- front/src/page/newsDetail.js
+++ front/src/page/newsDetail.js
@@ -5,10 +5,10 @@ import NewsCard from "./component/newsCard";
 
 const NewsDetail = () => {
   const [selectedArticle] = useState({
-    title: "애플 주가, AI 전략 공개 영향 7% 급등 사상 최고치…200불 돌파",
+    title: "'AI 장착' 애플, MS 제치고 5개월 만에 장중 시총 1위 탈환",
     author: "에이스일보",
     publishedAt: "2024-06-12",
-    content: `(로스앤젤레스=연합뉴스) 임미나 특파원 = 애플이 자체 기기에 탑재될 인공지능(AI) 기능을 공개한 다음 날인 11일(현지시간) 주가가 급등해 사상 최고치를 경신했다.
+    summary: `(로스앤젤레스=연합뉴스) 임미나 특파원 = 애플이 자체 기기에 탑재될 인공지능(AI) 기능을 공개한 다음 날인 11일(현지시간) 주가가 급등해 사상 최고치를 경신했다.
 
                     이날 뉴욕증시에서 애플 주가는 전장보다 7.26% 오른 207.15달러에 마감했다.
 
@@ -17,6 +17,7 @@ const NewsDetail = () => {
                     이날 뉴욕증시에서 애플 주가는 전장보다 7.26% 오른 207.15달러에 마감했다.
 
                     마감 직전에는 207.16달러까지 올라 52주 신고가를 새로 썼다.`,
+    link: "https://www.yna.co.kr/view/AKR20240613001600091?input=1195m",
     recommendations: [
       { label: "긍정", percentage: 0.4 },
       { label: "부정", percentage: 99.1 },
@@ -45,7 +46,7 @@ const NewsDetail = () => {
         "https://imgnews.pstatic.net/image/277/2024/06/13/0005431172_001_20240613090716062.jpg?type=w647",
       title: "삼성전자 노사, 오늘 대화 재개…갈등 봉합 여부 주목",
       publishedAt: "2024.06.13 09:05",
-      content:
+      summary:
         "삼성전자 노사가 13일 오전 10시부터 서울 서초사옥 인근 모처에서 만나 본교섭 일정과 논의 방향 등에 대해 의견을 나눈다.",
     },
     {
@@ -53,7 +54,7 @@ const NewsDetail = () => {
         "https://imgnews.pstatic.net/image/421/2024/06/13/0007598064_001_20240613061310646.jpg?type=w647",
       title: "'갓비디아 수혜주' 한미반도체, LG전자 시총 제치며 '고공행진'",
       publishedAt: " 2024.06.13 06:12",
-      content:
+      summary:
         "AI 반도체인 고대역폭메모리(HBM) 후공정에 필수적인 열압착(Thermal Compression) 본더를 생산하는 한미반도체 주가가 역대 최고가를 또 한 번 갈아치우며 LG전자 시가총액을 제쳤다.",
     },
   ];
@@ -66,10 +67,12 @@ const NewsDetail = () => {
         <h2 className="news-detail__title">{selectedArticle.title}</h2>
         <p className="news-detail__author">{selectedArticle.author}</p>
 
-        <p className="news-detail__content"> {selectedArticle.content} </p>
+        <p className="news-detail__content"> {selectedArticle.summary} </p>
         <p className="news-detail__date">{selectedArticle.publishedAt}</p>
 
-        <button className="news-detail__button">기사 본문 보러가기</button>
+        <a className="news-detail__button" href={selectedArticle.link}>
+          기사 본문 보러가기
+        </a>
         <div className="news-detail__recommendations">
           <h3>이 뉴스를 추천할게요</h3>
           <ul>
@@ -100,7 +103,7 @@ const NewsDetail = () => {
                 key={index}
                 image={article.image}
                 title={article.title}
-                content={article.content}
+                summary={article.summary}
                 publishedAt={article.publishedAt}
               />
             ))}
