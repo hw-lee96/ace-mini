af1586d1ad601e134f40efb2e93e270d63929442
HW <hyeongu0305@gmail.com>
2024-06-14 12:49:29 +0900

    불필요한 주석 제거

--- front/src/App.js
+++ front/src/App.js
@@ -11,38 +11,11 @@ import NewsMain from "./page/newsMain";
 
 import useStore from './commonStore'
 
-// const Home = () => (
-//     <div>
-//         <Link to="/">
-//             <button>HOME</button>
-//         </Link>
-//         <Link to="/exer/hj">
-//             <button>hj</button>
-//         </Link>
-//         <Link to="/exer/hw">
-//             <button>hw</button>
-//         </Link>
-//         <Link to="/exer/jh">
-//             <button>jh</button>
-//         </Link>
-//         <Link to="/exer/ra">
-//             <button>ra</button>
-//         </Link>
-//         <Link to="/exer/yk">
-//             <button>yk</button>
-//         </Link>
-//     </div>
-// )
-
 const App = () => {
   return useRoutes([
     { path: "/", element: <NewsMain /> },
     { path: "/list", element: <NewsList /> },
     { path: "/:id", element: <NewsDetail /> },
-    // { path: '/exer/hw', element: <Todo2 /> },
-    // { path: '/exer/jh', element: <Todo3 /> },
-    // { path: '/exer/ra', element: <Todo4 /> },
-    // { path: '/exer/yk', element: <Todo5 /> },
   ]);
 };
 
