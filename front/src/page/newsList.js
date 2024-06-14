0e0b30c60b26825927df8704e1bec1c1467d97e1
HW <hyeongu0305@gmail.com>
2024-06-14 12:19:43 +0900

    리스트 화면에서 상세화면 이동 로직 추가

--- front/src/page/newsList.js
+++ front/src/page/newsList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './newsList.css'
+import { Link } from 'react-router-dom'
 
 const NewsList = () => {
     const [newsList, setNewsList] = useState([])
@@ -33,30 +34,42 @@ const NewsList = () => {
 
     return (
         <div>
-            <div className='news-filter'>
-                <div className={type == '' ? 'active' : ''} onClick={ getAllNewsList }>전체</div>
-                <div className={type == 'positive' ? 'active' : ''} onClick={() => filter('positive')}>긍정</div>
-                <div className={type == 'neutral' ? 'active' : ''} onClick={() => filter('neutral')}>중립</div>
-                <div className={type == 'negative' ? 'active' : ''} onClick={() => filter('negative')}>부정</div>
+            <div className="news-filter">
+                <div className={type == '' ? 'active' : ''} onClick={getAllNewsList}>
+                    전체
+                </div>
+                <div className={type == 'positive' ? 'active' : ''} onClick={() => filter('positive')}>
+                    긍정
+                </div>
+                <div className={type == 'neutral' ? 'active' : ''} onClick={() => filter('neutral')}>
+                    중립
+                </div>
+                <div className={type == 'negative' ? 'active' : ''} onClick={() => filter('negative')}>
+                    부정
+                </div>
             </div>
 
             <div>
                 {newsList.map((news, i) => {
                     return (
-                        <div key={i} className="item compBg">
-                            <div className="img-box">
-                                <img src={news.img} alt=''/>
-                            </div>
-                            <div className="article-container">
-                                <div>
-                                    <div className="title">{news.title}</div>
-                                    <div className="content content-line-clamp">
-                                        <span className="gray-color content-font">📝[한 줄 요약]{news.summary}</span>
-                                    </div>
+                        <Link to={`/${news.id}`} className="ftColor">
+                            <div key={i} className="item compBg">
+                                <div className="img-box">
+                                    <img src={news.img} alt="" />
                                 </div>
-                                <div className="date-font">{news.date}</div>
+                                <div className="article-container">
+                                    <div>
+                                        <div className="title">{news.title}</div>
+                                        <div className="content content-line-clamp">
+                                            <span className="gray-color content-font">
+                                                📝[한 줄 요약]{news.summary}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className="date-font">{news.date}</div>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     )
                 })}
             </div>
