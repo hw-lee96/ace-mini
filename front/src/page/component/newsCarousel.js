47dd5644db4752d3a54e4e2ee592e04a8f52bbee
HW <hyeongu0305@gmail.com>
2024-06-14 16:47:18 +0900

    메인 화면 > 리스트 > 상세화면으로 연결되는 기능 추가

--- front/src/page/component/newsCarousel.js
+++ front/src/page/component/newsCarousel.js
@@ -1,48 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "./newsCarousel.css";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import './newsCarousel.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-import NewsCard from "./newsCard";
+import NewsCard from './newsCard'
+import useStore from '../../commonStore'
 
 async function getArticles(type) {
     try {
-        const response = await axios.get(`api/news/ranking/${type}`);
-        return response.data;
-
+        const response = await axios.get(`api/news/ranking/${type}`)
+        return response.data
     } catch (error) {
-        console.error('Error fetching data:', error);
-        return []; 
+        console.error('Error fetching data:', error)
+        return []
     }
 }
 
 function NewsCarousel({ type }) {
     const [relatedArticles, setRelatedArticles] = useState([])
+    const { setNewsId, setIsOpen } = useStore()
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await getArticles(type);
+                const data = await getArticles(type)
                 if (data && data.length > 0) {
-                    setRelatedArticles(data);
+                    setRelatedArticles(data)
                 } else {
-                    console.warn('Fetched data is empty:', data);
+                    console.warn('Fetched data is empty:', data)
                 }
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error('Error fetching data:', error)
             }
-        };
+        }
 
-        fetchData();
-    }, []);
+        fetchData()
+    }, [])
 
     // 데이터가 비어있는 경우 처리
     if (relatedArticles.length === 0) {
-        return <div>Loading...</div>;
+        return <div>Loading...</div>
     }
 
     return (
@@ -62,18 +64,23 @@ function NewsCarousel({ type }) {
             >
                 {relatedArticles.map((article) => (
                     <SwiperSlide key={article._id}>
-                        <NewsCard
-                            img={article.img}
-                            title={article.title}
-                            summary={article.summary}
-                            date={article.date}
-                        />
+                        <Link to="/list" className="ftColor">
+                            <NewsCard
+                                img={article.img}
+                                title={article.title}
+                                summary={article.summary}
+                                date={article.date}
+                                onClick={() => {
+                                    setNewsId(article._id)
+                                    setIsOpen(true)
+                                }}
+                            />
+                        </Link>
                     </SwiperSlide>
                 ))}
             </Swiper>
         </div>
     )
 }
-
 
 export default NewsCarousel
