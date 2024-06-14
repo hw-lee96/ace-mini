70805fbb19a3d85c9ee68a81d3933d18b52e07ff
HW <hyeongu0305@gmail.com>
2024-06-14 10:19:57 +0900

    헤더 로고 클릭 시 메인화면으로 이동하도록 수정

--- front/src/page/newsMain.js
+++ front/src/page/newsMain.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, useRoutes, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './newsMain.css'
 
 import NewsCarousel from './component/newsCarousel'
