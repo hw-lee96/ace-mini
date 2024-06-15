8675b1005005f40ff913b948b03d754153a3039b
hong <icandogo1@naver.com>
2024-06-15 11:54:45 +0900

    메인 페이지 스타일

--- front/src/page/component/newsCard.js
+++ front/src/page/component/newsCard.js
@@ -1,63 +1,82 @@
 // src/components/NewsCard.js
-import React, { useState } from "react";
-import "./newsCard.css";
-import { useTheme } from "../../theme/themeProvider";
-import useStore from "../../commonStore";
+import React, { useState } from 'react'
+import './newsCard.css'
+import { useTheme } from '../../theme/themeProvider'
+import useStore from '../../commonStore'
 
 const NewsCard = ({ id, img, title, date, views, like, summary, onClick }) => {
   const truncateText = (text, limit) => {
     if (text && text.length > limit) {
-      return text.substring(0, limit) + "...";
+      return text.substring(0, limit) + '...'
     }
-    return text;
-  };
+    return text
+  }
 
-  const [ThemeMode] = useTheme();
+  const [ThemeMode] = useTheme()
 
   return (
-    <div className="news-card recBar" onClick={() => onClick(id)}>
-      <img src={img} alt={title} className="news-card__image" onError={(e) => e.target.src = "./static/img_not_found.jpg"}/>
+    <div
+      className="news-card recBar"
+      style={{ height: '487.8px' }}
+      onClick={() => onClick(id)}
+    >
+      <img
+        src={img}
+        alt={title}
+        className="news-card__image"
+        style={{ objectFit: 'fill' }}
+        onError={(e) => (e.target.src = './static/img_not_found.jpg')}
+      />
       <div className="news-card__content">
-        <h4 className="news-card__title">{title}</h4>
-        <p className="news-card__snippet">{truncateText(summary, 50)}</p>
+        <h4
+          className="news-card__title"
+          style={{
+            fontSize: '23.4px',
+            lineHeight: '28.6px',
+            color: 'rgba(44,45,46,1)',
+          }}
+        >
+          {title}
+        </h4>
+        <p className="news-card__snippet"> {truncateText(summary, 100)}</p>
         <div className="date_like">
           <p className="news-card__date">{date}</p>
-          {ThemeMode === "dark" ? (
+          {ThemeMode === 'dark' ? (
             <img
               className="news-card__views_img"
               src="./static/view-dark.png"
               alt=""
-              onError={(e) => e.target.src = "./static/img_not_found.jpg"}
+              onError={(e) => (e.target.src = './static/img_not_found.jpg')}
             />
           ) : (
             <img
               className="news-card__views_img"
               src="./static/view-light.png"
               alt=""
-              onError={(e) => e.target.src = "./static/img_not_found.jpg"}
+              onError={(e) => (e.target.src = './static/img_not_found.jpg')}
             />
           )}
           <p className="news-card__views purCard">{views}</p>
-          {ThemeMode === "dark" ? (
+          {ThemeMode === 'dark' ? (
             <img
               className="news-card__like_img"
               src="./static/heart-dark.png"
               alt=""
-              onError={(e) => e.target.src = "./static/img_not_found.jpg"}
+              onError={(e) => (e.target.src = './static/img_not_found.jpg')}
             />
           ) : (
             <img
               className="news-card__like_img"
               src="./static/heart-light.png"
               alt=""
-              onError={(e) => e.target.src = "./static/img_not_found.jpg"}
+              onError={(e) => (e.target.src = './static/img_not_found.jpg')}
             />
           )}
           <p className="news-card__like purCard">{like}</p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NewsCard;
+export default NewsCard
--- front/src/page/component/newsCarousel.js
+++ front/src/page/component/newsCarousel.js
@@ -1,98 +1,97 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "./newsCarousel.css";
-import axios from "axios";
+import React, { useState, useEffect, useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import './newsCarousel.css'
+import axios from 'axios'
 
-import NewsCard from "./newsCard";
+import NewsCard from './newsCard'
 
 async function getArticles(type) {
-    try {
-        const response = await axios.get(`api/news/ranking/${type}`);
-        console.log(response.data)
-        return response.data;
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return [];
-    }
+  try {
+    const response = await axios.get(`api/news/ranking/${type}`)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return []
+  }
 }
 
 function NewsCarousel({ type }) {
-    const prevRef = useRef(null);
-    const nextRef = useRef(null);
+  const prevRef = useRef(null)
+  const nextRef = useRef(null)
 
-    const [relatedArticles, setRelatedArticles] = useState([])
+  const [relatedArticles, setRelatedArticles] = useState([])
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await getArticles(type);
-                if (data && data.length > 0) {
-                    setRelatedArticles(data);
-                } else {
-                    console.warn('Fetched data is empty:', data);
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    // 데이터가 비어있는 경우 처리
-    if (relatedArticles.length === 0) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getArticles(type)
+        if (data && data.length > 0) {
+          setRelatedArticles(data)
+        } else {
+          console.warn('Fetched data is empty:', data)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
     }
 
-    return (
-        <div className="swiper-container">
-            <div className="swiper-wrapper">
-                <div className="swiper-button-prev custom-nav-button" ref={prevRef}>
-                    <img src="/static/prevBtn.png" alt="Previous" />
-                </div>
-                <div className="swiper-button-next custom-nav-button" ref={nextRef}>
-                    <img src="/static/nextBtn.png" alt="Next" />
-                </div>
-                <Swiper
-                    modules={[Navigation, Autoplay]}
-                    loop={true}
-                    slidesPerView={1.9}
-                    spaceBetween={20}
-                    navigation={{
-                        prevEl: prevRef.current,
-                        nextEl: nextRef.current,
-                    }}
-                    onInit={(swiper) => {
-                        swiper.params.navigation.prevEl = prevRef.current;
-                        swiper.params.navigation.nextEl = nextRef.current;
-                        swiper.navigation.init();
-                        swiper.navigation.update();
-                    }}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 2500,
-                    }}
-                    style={{ width: '100%', height: '300px' }}
-                >
-                    {relatedArticles.map((article) => (
-                        <SwiperSlide key={article._id}>
-                                <NewsCard
-                                    img={article.img}
-                                    title={article.title}
-                                    summary={article.summary}
-                                    date={article.date}
-                                />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
+    fetchData()
+  }, [])
+
+  // 데이터가 비어있는 경우 처리
+  if (relatedArticles.length === 0) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="swiper-container">
+      <div className="swiper-wrapper">
+        <div className="swiper-button-prev custom-nav-button" ref={prevRef}>
+          <img src="/static/prevBtn.png" alt="Previous" />
         </div>
-    )
+        <div className="swiper-button-next custom-nav-button" ref={nextRef}>
+          <img src="/static/nextBtn.png" alt="Next" />
+        </div>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          loop={true}
+          slidesPerView={1.9}
+          spaceBetween={20}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onInit={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current
+            swiper.params.navigation.nextEl = nextRef.current
+            swiper.navigation.init()
+            swiper.navigation.update()
+          }}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+          }}
+          style={{ width: '100%', height: '300px', borderRadius: '15px' }}
+        >
+          {relatedArticles.map((article) => (
+            <SwiperSlide key={article._id}>
+              <NewsCard
+                img={article.img}
+                title={article.title}
+                summary={article.summary}
+                date={article.date}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  )
 }
 
 export default NewsCarousel
