7beffd13c04d6fdeece4c3daf9beca87e97f9133
HW <hyeongu0305@gmail.com>
2024-06-17 11:51:12 +0900

    랭킹 조회 api에 views, like count 추가

--- back/route/news_ranking.py
+++ back/route/news_ranking.py
@@ -12,7 +12,7 @@ news_data = db['news']
 
 @router.get("/ranking/like")
 async def get_most_liked_news():
-    cursor = news_data.find({}, {"title": 1, "summary": 1, "date": 1, "img": 1, "like": 1}).sort("like", DESCENDING).limit(5)
+    cursor = news_data.find({}, {"title": 1, "summary": 1, "date": 1, "img": 1, "views": 1, "like": 1}).sort("like", DESCENDING).limit(5)
 
     most_liked_news_data = [
             {**doc, '_id': str(doc['_id'])} for doc in cursor
@@ -21,7 +21,7 @@ async def get_most_liked_news():
 
 @router.get("/ranking/view")
 async def get_most_viewed_news():
-    cursor = news_data.find({}, {"title": 1, "summary": 1, "date": 1, "img": 1, "views": 1}).sort("views", DESCENDING).limit(5)
+    cursor = news_data.find({}, {"title": 1, "summary": 1, "date": 1, "img": 1, "views": 1, "like": 1}).sort("views", DESCENDING).limit(5)
 
     most_viewed_news_data = [
             {**doc, '_id': str(doc['_id'])} for doc in cursor
